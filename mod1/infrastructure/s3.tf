da1a894c9dc30b2ad8633d5811523f221da17dfa
RickLeite <movereconhecimento@gmail.com>
2023-03-29 00:04:10 -0300

    fixing terraform /undeclared resources/ error

--- mod1/infrastructure/s3.tf
+++ mod1/infrastructure/s3.tf
@@ -11,12 +11,12 @@ resource "aws_s3_bucket" "dl-pipe" {
 }
 
 resource "aws_s3_bucket_acl" "bucket_acl" {
-  bucket = aws_s3_bucket.datalake.id
+  bucket = aws_s3_bucket.dl-pipe.id
   acl    = "private"
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "datalake-encript" {
-  bucket = aws_s3_bucket.datalake.id
+  bucket = aws_s3_bucket.dl-pipe.id
 
   rule {
     apply_server_side_encryption_by_default {
