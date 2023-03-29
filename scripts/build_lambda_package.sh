6eb2b760be6f0a1ee685e3abdaaee31c7ec2fb97
RickLeite <movereconhecimento@gmail.com>
2023-03-29 16:30:54 -0300

    lambda package

--- scripts/build_lambda_package.sh
+++ scripts/build_lambda_package.sh
@@ -1,52 +1,51 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash 
 
 cd infrastructure
 
+# Declara variavel para reutilização nas validações do diretório
 PACKAGE="package"
 
+# Cria o diretório e instala as dependências da função lambda
 if [ -d $PACKAGE ]
 then
-  echo "O Diretório $PACKAGE já existe."
+	echo "O Diretório "$PACKAGE" já existe."
 else
-    echo "===>"
-    echo "Criando o diretório $PACKAGE"
-    mkdir $PACKAGE
-    echo "O diretório $PACKAGE foi criado."
-    echo "<==="
+	echo "============================================="
+	echo "Criando o diretório "$PACKAGE"..."
+	mkdir $PACKAGE
+	echo "O diretório "$PACKAGE" foi criado."
+	echo "============================================="
 fi
 
-# Localiza o requirements com as dependências do projeto
+# Declara variavel que localiza o requirements com as dependências do projeto
 FILE_REQUIREMENTS=../etl/lambda_requirements.txt
 
-# Verifica se o arquivo lambda_requirements.txt existe
+# Verifica se o arquivo lambda_requirements existe
 if [ -f $FILE_REQUIREMENTS ]
 then
-    echo "===>"
-    echo "Instalando as dependências localizadas no arquivo "$FILE_REQUIREMENTS""
-    pip install --target ./package -r $FILE_REQUIREMENTS
-    echo "As dependências foram instaladas."
-    echo "<==="
-    exit 1
+	echo "============================================="
+	echo "Instalando dependências localizadas no "$FILE_REQUIREMENTS""
+	pip install --target ./package -r $FILE_REQUIREMENTS
+	echo "Dependências instaladas com sucesso."
+	echo "============================================="	
 fi
+
 
 cd $PACKAGE
 
-# Localiza a função lambda para reutilização
+# Declara variavel que localiza a função lambda para reutilização no código.
 LAMBDA_FUNCTION=../../etl/lambda_function.py
 
 # Verifica se o arquivo lambda_function.py existe
 if [ -f $LAMBDA_FUNCTION ]
 then
-    echo "===>"
-    echo "Copiando função Handler do arquivo "$LAMBDA_FUNCTION""
-    cp $LAMBDA_FUNCTION .
-    echo "O arquivo "$LAMBDA_FUNCTION" foi copiado."
-    echo "===="
-    echo "Compactando o arquivo lambda_function_payload.zip"
-    zip -r9 ../lambda_function_payload.zip .
-    echo "O arquivo lambda_function_payload.zip foi compactado."
-    echo "<==="
-    exit 1
+	echo "============================================="
+	echo "Copiando função Handler..."
+	cp $LAMBDA_FUNCTION .
+	echo "Compactando arquivo lambda_function_payload.zip"
+	zip -r9 ../lambda_function_payload.zip . #Compacta o pacote para o deploy
+	echo "Arquivo compactado com sucesso!"
+	echo "============================================="
 fi
 
 cd ..
